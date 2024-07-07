eac0db6d29e61ef4d89310701fa88f2802e6ebd7
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-07-07 21:40:41 +0530

    reduxtoolkit configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-
+import React, { useState , useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice';
+import { Header,Footer } from './components';
+import { Outlet } from 'react-router-dom';
 const App = () => {
+
+  const [loading,setLoading]=useState(true);
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((useData)=>{
+      if(useData){
+        dispatch(login({useData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
   console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div>App with appwrite</div>
-  )
+  // conditional rendering
+
+  return !loading? (
+    <div className='min-h-screen flex flex-wrap text-center bg-slate-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO:{/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
