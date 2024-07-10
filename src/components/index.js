bac04bddb720cb10251033fc4863ac595ec759a5
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-07-10 15:03:47 +0530

    bulding pages and routes

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,27 @@ import Container from "../container/Container";
 import Logo from './Logo';
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-
+import Signup from './Signup';
+import Login from './Login'
+import PostCard from './PostCard';
+import Select from './Select'
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
+import Button from "./Button";
+import Input from "./Input";
 export {
-    Header,Footer,Container,Logo,LogoutBtn,RTE
+    Header,
+    Footer,
+    Container,
+    Logo, 
+    LogoutBtn, 
+    Button,
+    Input,
+    RTE, 
+    Signup, 
+    Login, 
+    Select, 
+    PostCard, 
+    PostForm, 
+    AuthLayout
 }
